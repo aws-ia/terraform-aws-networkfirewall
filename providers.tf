075208e84a9be037bc09904c300db156b98ddfd3
Pablo Sánchez Carmona <pablosc@amazon.es>
2022-10-04 14:51:00 +0200

    required_providers 1.3.x & remove experiments block

--- providers.tf
+++ providers.tf
@@ -4,14 +4,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0.0, < 5.0.0"
+      version = ">= 3.73.0"
     }
     awscc = {
       source  = "hashicorp/awscc"
-      version = ">= 0.24.0"
+      version = ">= 0.15.0"
     }
   }
 
-  required_version = ">= 1.0.7"
-  experiments      = [module_variable_optional_attrs]
+  required_version = ">= 1.3.0"
 }
